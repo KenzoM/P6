57803a7fe81328e44d5852e0ad02f45810f9ae4e
Kenzo <kenzom954@gmail.com>
2015-06-10 13:44:00 -0600

    Finished Step 11 on test class menu-hidden

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -56,6 +56,10 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The Menu',function(){
+        it('is hidden by default', function(){
+            expect($('body').hasClass("menu-hidden")).toBe(true);
+        })
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -63,11 +67,17 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
+
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+         
+    })
+
+
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
